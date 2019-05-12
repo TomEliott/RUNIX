77ba32ecc597c9f17135b03fe0036fcb1887cdd0
TomEliott <thz2601@me.com>
2019-05-12 12:43:06 +0200

    StatsActivity, Statistics and Graph are now linked

--- app/src/main/java/com/griffithcollege/runix/Graph.java
+++ app/src/main/java/com/griffithcollege/runix/Graph.java
@@ -11,8 +11,6 @@ import java.util.LinkedList;
 
 public class Graph extends View
 {
-    float max_time = 40; //example (to delete)
-
     public Graph(Context context)
     {
         super(context);
@@ -50,60 +48,69 @@ public class Graph extends View
         canvas.drawLine(0,0,0, 500, paint); //Y axis
         canvas.drawLine(0,0, getWidth(),0, paint); //X axis
 
-
         draw_xPoint(canvas);
         draw_yPoint(canvas);
+
+        // TESTS
+        StatsActivity stats = new StatsActivity();
+        LinkedList<Integer> times = stats.getTimes();
+        LinkedList<Integer> speeds = stats.getSpeeds();
+        float max_time = stats.getMaxTime();
+        addLink(canvas, times, speeds, max_time);
 
         //--- TEST ---//
         //TEST_TEST_TEST(canvas);
         // <=>
+        /*
         LinkedList<Integer> times = new LinkedList<>();
         LinkedList<Integer> speeds = new LinkedList<>();
         times.add(0); times.add(10); times.add(20); times.add(30); times.add(40);
         speeds.add(0); speeds.add(5); speeds.add(7); speeds.add(3); speeds.add(10);
         addLink(canvas, times, speeds);
+        */
     }
 
-    public void TEST_TEST_TEST(Canvas canvas) // TO DELETE AFTER
+    /*
+    public void TEST_TEST_TEST(Canvas canvas, float max_time) // TO DELETE AFTER
     {
-        max_time = 40; // temps total = 40s
+        //max_time = 40; // temps total = 40s
 
-        setPoint(canvas, 0, 0); // A
+        setPoint(canvas, 0, 0, max_time); // A
 
-        setPoint(canvas, 10, 5); //B
-        setLinkAB(canvas, 0, 0, 10, 5);
+        setPoint(canvas, 10, 5, max_time); //B
+        setLinkAB(canvas, 0, 0, 10, 5, max_time);
 
-        setPoint(canvas, 20, 7);
-        setLinkAB(canvas, 10, 5, 20, 7);
+        setPoint(canvas, 20, 7, max_time);
+        setLinkAB(canvas, 10, 5, 20, 7, max_time);
 
-        setPoint(canvas, 30, 3);
-        setLinkAB(canvas, 20, 7, 30, 3);
+        setPoint(canvas, 30, 3, max_time);
+        setLinkAB(canvas, 20, 7, 30, 3, max_time);
 
-        setPoint(canvas, 40, 10);
-        setLinkAB(canvas, 30, 3, 40, 10);
+        setPoint(canvas, 40, 10, max_time);
+        setLinkAB(canvas, 30, 3, 40, 10, max_time);
     }
+    */
 
-    public void addLink(Canvas canvas, LinkedList<Integer> Times, LinkedList<Integer> Speeds)
+    public void addLink(Canvas canvas, LinkedList<Integer> Times, LinkedList<Integer> Speeds, float max_time)
     {
         // Times and Speeds must be the same size !
-        setPoint(canvas, Times.get(0), Speeds.get(0)); // Draw the first point
+        setPoint(canvas, Times.get(0), Speeds.get(0), max_time); // Draw the first point
 
         int size = Times.size();
-        setPoint(canvas, Times.get(0), Speeds.get(0));
         for (int p = 1; p < size; p++)
         {
-            setPoint(canvas, Times.get(p), Speeds.get(p));
-            setLinkAB(canvas, Times.get(p - 1), Speeds.get(p - 1), Times.get(p), Speeds.get(p));
+            setPoint(canvas, Times.get(p), Speeds.get(p), max_time);
+            setLinkAB(canvas, Times.get(p - 1), Speeds.get(p - 1), Times.get(p), Speeds.get(p), max_time);
         }
     }
 
-    public void setLinkAB(Canvas canvas, int timeA, int speedA, int timeB, int speedB)
+    public void setLinkAB(Canvas canvas, int timeA, int speedA, int timeB, int speedB, float max_time)
     {
         float coeff = getWidth() / max_time;
         canvas.drawLine(timeA*coeff, speedA*50, timeB*coeff, speedB*50, paint);
     }
 
-    public void setPoint(Canvas canvas, int time, int speed)
+    public void setPoint(Canvas canvas, int time, int speed, float max_time)
     {
         float coeff = getWidth() / max_time;
         float position_x = time*coeff;
